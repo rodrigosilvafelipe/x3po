0d6ebed06eac1586da6cf0ee2296a855a44f1e96
rodrigosilvafelipe <65677572+rodrigosilvafelipe@users.noreply.github.com>
2023-10-09 19:15:13 -0300

    Correção de bug

--- moveArquivos.py
+++ moveArquivos.py
@@ -11,12 +11,28 @@ while True:
         # Lista todos os arquivos na pasta de origem
         arquivos = os.listdir(origem)
         
-        # Move cada arquivo para a pasta de destino
-        for arquivo in arquivos:
-            shutil.move(os.path.join(origem, arquivo), os.path.join(destino, arquivo))
+        # Verifica se ainda há arquivos para mover
+        if arquivos:
+            # Seleciona um arquivo para mover
+            arquivo = arquivos[0]
+            caminho_origem = os.path.join(origem, arquivo)
+            caminho_destino = os.path.join(destino, arquivo)
+            
+            # Move o arquivo para a pasta de destino
+            shutil.move(caminho_origem, caminho_destino)
+            
+            # Espera 5 segundos antes de mover o próximo arquivo
+            time.sleep(5)
+        else:
+            # Se não há arquivos para mover, espera 5 segundos antes de verificar novamente
+            time.sleep(5)
+
+        # # Move cada arquivo para a pasta de destino
+        # for arquivo in arquivos:
+        #     shutil.move(os.path.join(origem, arquivo), os.path.join(destino, arquivo))
         
-        # Espera 10 segundos antes de mover os próximos arquivos
-        time.sleep(10)
+        # # Espera 10 segundos antes de mover os próximos arquivos
+        # time.sleep(10)
     
     except Exception as e:
         # print(f"Erro: {str(e)}")
